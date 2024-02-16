2409c09b58ed0cfb7f4ab5ede87e2470789e9298
건호 <111233054+heogeonho@users.noreply.github.com>
2024-02-16 18:53:54 +0900

    [Silver V] Title: 카드1, Time: 196 ms, Memory: 16096 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Queue<Integer> queue = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+        int i = 1;
+        while (!queue.isEmpty()) {
+            int k = queue.remove();
+            if (i % 2 == 0) {
+                queue.add(k);
+            } else {
+                System.out.print(k+" ");
+            }
+            i++;
+        }
+    }
+}
