246a0e407405300ce227d660dc9c3cbad1973964
건호 <111233054+heogeonho@users.noreply.github.com>
2024-03-22 18:39:20 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 344 ms, Memory: 110620 KB -BaekjoonHub

--- 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.py
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.py
@@ -3,19 +3,17 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 arr = list(map(int, input().split()))
+count = 0
 
-
-def bubble_sort(N, K, arr) :
-    count = 0
-    result = -1
-    for i in range(N-1, 0, -1) :
-        
-        for j in range(i):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                count += 1
-                if count == K:
-                    result = f'{arr[j]} {arr[j+1]}'
-    return result
-
-print (bubble_sort(N, K, arr))
+for i in range(N-1, 0, -1):
+    for j in range(i):
+        if(arr[j] > arr[j+1]):
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            count += 1
+            if (K == count):
+                print(f'{arr[j]} {arr[j+1]}')
+                break
+    if (K == count):
+        break
+if(K > count):
+    print(-1)
