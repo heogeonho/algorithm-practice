1ee894db8b16f494a5366670cf40c15a08f330ee
건호 <111233054+heogeonho@users.noreply.github.com>
2024-03-26 03:01:20 +0900

    [Bronze I] Title: 팰린드롬수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    str = input().strip()
+    if str == "0":
+        break
+    if str == str[::-1]:
+        print("yes")
+    else :
+        print("no")
+        
