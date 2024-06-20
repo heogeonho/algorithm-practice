15921f5c0be1b34dd07220cae952e3832e328cf5
건호 <111233054+heogeonho@users.noreply.github.com>
2024-06-20 11:47:59 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 72 ms, Memory: 12476 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,26 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{	
+public class Main {	
 	static String delimiter = "";
 	
 	static void chatbot(int n) {
-		String tempUnderbar = delimiter;
-		System.out.println(tempUnderbar+"\"재귀함수가 뭔가요?\"");
+		String temp = delimiter;
+		StringBuilder sb=new StringBuilder();
 		if(n==0) {
-			System.out.println(tempUnderbar+"\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-			System.out.println(tempUnderbar+"라고 답변하였지.");
+			sb.append(temp).append("\"재귀함수가 뭔가요?\"\n");
+			sb.append(temp).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+			sb.append(temp).append("라고 답변하였지.");
+			System.out.println(sb.toString());
 			return;
 		}
-		System.out.println(tempUnderbar+"\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-		System.out.println(tempUnderbar+"마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-		System.out.println(tempUnderbar+"그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+		sb.append(temp).append("\"재귀함수가 뭔가요?\"\n");
+		sb.append(temp).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+		sb.append(temp).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+		sb.append(temp).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+		System.out.println(sb.toString());
 		
 		delimiter += "____";
 		
 		chatbot(n-1);
 		
-		System.out.println(tempUnderbar+"라고 답변하였지.");
+		System.out.println(temp+"라고 답변하였지.");
 	}
 	
 	public static void main(String[] args) throws Exception{
