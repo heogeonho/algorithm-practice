4f0338348ff264311b01316aa43226374a6358d0
건호 <111233054+heogeonho@users.noreply.github.com>
2024-06-25 21:36:05 +0900

    [Gold V] Title: 강의실 배정, Time: 692 ms, Memory: 76920 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -7,6 +7,7 @@ public class Main {
 		StringTokenizer st=null;
 		
 		int N=Integer.parseInt(br.readLine());
+		// 강의실이 미배정된 강의가 저장된 int 배열 a
 		int[][] a=new int[N][2];
 		for (int i=0; i<N; i++) {
 			st=new StringTokenizer(br.readLine()," ");
@@ -15,6 +16,7 @@ public class Main {
 		}
 //		for(int[] al:a) System.out.print(Arrays.toString(al)+" "); System.out.println();
 
+		//빠른 강의부터 집어넣기 위해 sort
 		Arrays.sort(a, (o1, o2)->{
 			int res=Integer.compare(o1[0], o2[0]);
 			if(res==0) {
@@ -24,8 +26,10 @@ public class Main {
 		});
 //		for(int[] al:a) System.out.print(Arrays.toString(al)+" "); System.out.println();
 		
+		//강의장 배정을 위한 우선순위큐
 		PriorityQueue<Integer> pq=new PriorityQueue<>();
 		
+		//가장 빠른 강의 배정하기
 		pq.offer(a[0][1]);
 		
 		for (int i=1; i<N; i++) {
