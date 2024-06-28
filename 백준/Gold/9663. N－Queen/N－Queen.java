e1b96bb74f19d0e8e9b29686a5b9a4c0b323ee49
건호 <111233054+heogeonho@users.noreply.github.com>
2024-06-28 11:43:41 +0900

    [Gold IV] Title: N-Queen, Time: 5528 ms, Memory: 11988 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, ans, col[];
+
+	static boolean isAvailable(int rowNo) {
+		for(int j=0; j<rowNo; j++) {
+			if(col[rowNo]==col[j] || Math.abs(col[rowNo]-col[j])==rowNo-j) return false; 
+		}
+		return true;
+	}
+	
+	static void nqueen(int rowNo) {
+		if(!isAvailable(rowNo-1)) return;
+		if(rowNo==N) {
+			ans++;
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			col[rowNo]=i;
+			nqueen(rowNo+1);
+		}
+	}
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		N=Integer.parseInt(br.readLine());
+		col=new int[N];
+		ans=0;
+		nqueen(0);
+		System.out.println(ans);
+		br.close();
+	}
+}
