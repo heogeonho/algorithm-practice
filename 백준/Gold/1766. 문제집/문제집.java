699b9a77ee3a946900352d2a345585454dad6ed8
건호 <111233054+heogeonho@users.noreply.github.com>
2024-07-02 11:05:19 +0900

    [Gold II] Title: 문제집, Time: 428 ms, Memory: 47568 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, M;
+	static List<Integer>[] prob;
+	static int[] indegree;
+	static StringBuilder sb=new StringBuilder();
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=null;
+		
+		//N 노드 수, M 간선 수 
+		st=new StringTokenizer(br.readLine()," ");
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		
+		//학생 키 받아넣기
+		prob=new List[N+1]; for(int i=1; i<N+1; i++) prob[i]=new ArrayList<>();
+		indegree=new int[N+1];
+		for(int i=0; i<M; i++) {
+			st=new StringTokenizer(br.readLine()," ");
+			int from=Integer.parseInt(st.nextToken());
+			int to=Integer.parseInt(st.nextToken());
+			prob[from].add(to);
+			indegree[to]++; // 진입 차수
+		}
+		
+		bfs();
+		System.out.println(sb);
+		br.close();
+	}
+
+	static void bfs() {
+		PriorityQueue<Integer> q=new PriorityQueue<>();
+		for(int i=1; i<N+1; i++) {
+			if(indegree[i]==0) q.offer(i); //0인거 찾아 큐에 넣기
+		}
+		while(!q.isEmpty()) {
+			int i=q.poll();
+			sb.append(i+" ");
+			for (int j:prob[i]) {
+				indegree[j]--;
+				if(indegree[j]==0) q.offer(j);
+			}
+		}
+	}
+}
