3d72c55aed00a23bda9ffa6da49d5cc56d933d8c
건호 <111233054+heogeonho@users.noreply.github.com>
2024-07-16 10:43:59 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 304 ms, Memory: 24904 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+/*
+<메인 아이디어>
+07/15 16:50
+먼저 작은수 둘을 꺼내 비교해야 한다고 생각
+pq를 활용할 것
+수 두개를 꺼냈을 때 pq가 빈다면 최종 값으로 선정 후 종료
+
+
+<배운점 및 생각 정리>
+예외처리에 대한 고민을 좀 더 신중하게 해야할 것 같다.
+입력값이 1개 2개 등과 같이 조금 주어지는 경우도 처리 방법을 고려해야 한다.
+ */
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq=new PriorityQueue<>();
+		int ans=0;
+		
+		for(int i=0; i<N; i++) {
+			pq.offer(Integer.parseInt(br.readLine()));
+		}
+		int temp=0;
+		while (true) {
+				if(pq.size()==1) {
+					ans=0;
+					break;
+				}
+				temp=pq.poll();
+				temp=temp+pq.poll();
+				ans+=temp;
+				if(pq.isEmpty()) break;
+				pq.offer(temp);
+		}
+		System.out.println(ans);
+		br.close();
+	}
+}
