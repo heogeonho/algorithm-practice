51bd500a49f9233836c470a150ba5d29273f71db
건호 <111233054+heogeonho@users.noreply.github.com>
2024-07-23 21:35:42 +0900

    [level 2] Title: 주식가격, Time: 16.05 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,33 +1,24 @@
 import java.util.*;
-import java.io.*;
 
 
 class Solution {
-    public static int[] solution(int[] prices) {
-    	Stack<int[]> stack=new Stack<>();
-    	int len=prices.length;
-    	int[] answer = new int[len];
-    	
-    	for(int i=0; i<len; i++) {	// 전체 prices 순회를 위한 반복문
-    		
-    		if(stack.isEmpty()) { 				// 비어있으면 값 넣고 다음 인덱스 처리
-    			stack.push(new int[] {i, prices[i]});
-    			continue;
-    		}
-    				
-			while(stack.peek()[1]>prices[i]) { // 스택이 안비어있을때만 수행
-				int[] tmp=stack.pop();
-				answer[tmp[0]]=i-tmp[0];
-				if(stack.isEmpty()) break;
-			}
-			stack.push(new int[] {i, prices[i]}); // 비교하고 결과값 산출 후 다음과정 넘어가기 전 push
-    	}
-   
-    	int n= stack.size();
-    	for (int i=0; i<n; i++) {	// push 과정 끝나고 남아있는 겂에 대한 처리
-    		int[] tmp=stack.pop();
-			answer[tmp[0]]=len-1-tmp[0];
-    	}
-        return answer;
+ public static int[] solution(int[] prices) {
+	     
+     
+               int s = prices.length;
+        int[] d = new int[s];
+        
+        for (int i = 0; i < s; i++) {
+            int temp = 0;
+            for (int j = i + 1; j < s; j++) {
+                temp++;
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+            }
+            d[i] = temp;
+        }
+        
+        return d;
     }
 }
