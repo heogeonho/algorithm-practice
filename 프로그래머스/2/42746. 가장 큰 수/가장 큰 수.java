d1b8eb743af07b0e5c6263216818febcb820a60a
건호 <111233054+heogeonho@users.noreply.github.com>
2024-07-29 11:04:24 +0900

    [level 2] Title: 가장 큰 수, Time: 2889.67 ms, Memory: 393 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+		static String solution(int[] numbers) {
+		int len=numbers.length;
+		
+		// String 배열 생성
+		String[] strings=new String[len];
+		for(int i=0; i<len; i++) {
+			strings[i]=String.valueOf(numbers[i]);
+		}
+		
+		// Stirng 배열에 대한 정렬
+		Arrays.sort(strings, (o1, o2)-> (o2 + o1).compareTo(o1 + o2));
+		
+		String answer="";
+		if(strings[0].equals("0")) return "0";
+		
+		for(int i=0; i<len; i++) {
+			answer+=strings[i];
+		}
+		
+        return answer;
+    }
+}
