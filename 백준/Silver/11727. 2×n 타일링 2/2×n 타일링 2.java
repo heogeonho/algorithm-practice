df61abb658669b7741d2dc92d617453609cd45e9
건호 <111233054+heogeonho@users.noreply.github.com>
2024-08-26 22:07:35 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 64 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,24 +1,31 @@
 import java.util.*;
 import java.io.*;
-/*
-DP 알고리즘에 대한 이해 첫걸음
-점화식 구하는 방식에 대한 고민 -> 블럭의 길이에 따라 점화식 세우기 접근 등등
-추가적으로 %10007을 해주는 이유와 오버플로우에 대한 고려를 해보는 계기가 된 문제
- */
+
 public class Main {
-	public static void main(String[] args) throws Exception {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int[] dp=new int[n+4];
-		
-		dp[0]=0;
-		dp[1]=1;
-		dp[2]=3;
-		
-		for(int i=3; i<=n; i++) {
-			dp[i]=(dp[i-1]+(dp[i-2]*2))%10007;
-		}
-		
-		System.out.println(dp[n]);
-	}
+    
+    static int[] memoization;
+    static int n;
+    
+    public static void main(String[] args) throws Exception {
+        // n = (n-1) + {(n-2)*2}
+        // Top Down
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        
+        memoization = new int[n+1];
+        Arrays.fill(memoization, -1); // -1로 초기화!
+
+        System.out.println(dpSol(n));
+    }
+
+    static int dpSol(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 3;
+
+        if(memoization[n] != -1) {
+            return memoization[n];
+        }
+        memoization[n] = (dpSol(n-1) + (dpSol(n-2) * 2)) % 10007;
+        return memoization[n];
+    }
 }
