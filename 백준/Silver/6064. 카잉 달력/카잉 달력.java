ed1df8da400bf3c4916312b537b8c240d947c97d
건호 <111233054+heogeonho@users.noreply.github.com>
2025-01-04 18:47:44 +0900

    [Silver I] Title: 카잉 달력, Time: 856 ms, Memory: 15996 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int answer = -1;
+            int k = 0;
+            int cnt = 0;
+            while (k < lcm(M, N)) {
+                k = M * cnt + x;
+                if ((k - 1) % N + 1 == y) {
+                    answer = k;
+                    break;
+                }
+                cnt++;
+            }
+            sb.append(answer).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int lcm(int a, int b) {
+        return a * (b / gcd(a, b)); // LCM 공식: (a * b) / GCD(a, b)
+
+    }
+}
