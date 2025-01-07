5b060de15f330c84a6cc1e37121c7d706dda70dc
건호 <111233054+heogeonho@users.noreply.github.com>
2025-01-07 20:47:35 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 76 ms, Memory: 12316 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -5,31 +5,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        
-        Queue<Integer> queue = new LinkedList<>();
-        StringBuilder sb = new StringBuilder();
-        
+
+        List<Integer> list = new ArrayList<>();
         for (int i = 1; i <= N; i++) {
-            queue.add(i);
+            list.add(i);
         }
-        
+
+        StringBuilder sb = new StringBuilder();
         sb.append("<");
-        
-        while (!queue.isEmpty()) {
-            for (int i = 0; i < K - 1; i++) {
-                queue.add(queue.poll());
-            }
-            sb.append(queue.poll());
-            
-            if (!queue.isEmpty()) {
+
+        int idx = 0; // 제거할 인덱스
+
+        while (!list.isEmpty()) {
+            idx = (idx + K - 1) % list.size(); // K번째 요소의 인덱스 계산
+            sb.append(list.remove(idx)); // 해당 요소 제거
+
+            if (!list.isEmpty()) {
                 sb.append(", ");
             }
         }
-        
+
         sb.append(">");
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
