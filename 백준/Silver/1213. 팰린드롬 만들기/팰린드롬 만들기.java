db03adcf4edd0210c9aaf896345092a9f5cb3cb9
건호 <111233054+heogeonho@users.noreply.github.com>
2025-02-03 02:50:01 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -3,20 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
         String input = br.readLine();
+        int len = input.length();
         int[] arr = new int[26];
 
-        // 문자 개수 카운트 & 홀수 개 문자 체크
-        int oddCount = 0, oddIndex = -1;
-        for (char c : input.toCharArray()) {
-            int idx = c - 'A';
-            arr[idx]++;
-            if (arr[idx] % 2 == 1) oddCount++;
-            else oddCount--;
+        for (int i = 0; i < len; i++) {
+            int index = input.charAt(i) - 'A';
+            arr[index]++;
         }
 
+        int oddIndex = -1;
+        int oddCount = 0;
+
+        // 홀수 개수 확인
+        // * 홀수 개수 확인만 하면 되는 이유 -> 짝수 길이일때 홀수가 나오면 무조건 홀수 개수 2개됨
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] % 2 == 1) {
+                oddCount++;
+                oddIndex = i;
+            }
+        }
+
+        // 팰린드롬을 만들 수 없는 경우
         if (oddCount > 1) {
             System.out.println("I'm Sorry Hansoo");
             return;
@@ -28,14 +40,21 @@ public class Main {
 
         for (int i = 0; i < 26; i++) {
             if (arr[i] > 0) {
+                // 절반만큼 추가
                 char c = (char) (i + 'A');
-                firstHalf.append(String.valueOf(c).repeat(arr[i] / 2));
+                for (int j = 0; j < arr[i] / 2; j++) {
+                    firstHalf.append(c);
+                }
 
-                if (arr[i] % 2 == 1) middleChar = c;
+                // 홀수 개수인 문자는 가운데 배치
+                if (arr[i] % 2 == 1) {
+                    middleChar = c;
+                }
             }
         }
 
         // 최종 결과 출력
         System.out.println(firstHalf.toString() + (middleChar != '\0' ? middleChar : "") + firstHalf.reverse().toString());
+        br.close();
     }
 }
