7ba2a622e57b90afbfa2c7f28de975e3db34a4a9
건호 <111233054+heogeonho@users.noreply.github.com>
2025-02-05 15:16:32 +0900

    [Silver I] Title: 신입 사원, Time: 792 ms, Memory: 298588 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -10,30 +10,29 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] list = new int[n+1];
 
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
-            int[][] memberList = new int[N][2];
-            for (int j = 0; j < N; j++) {
+            for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-                memberList[j][0] = Integer.parseInt(st.nextToken());
-                memberList[j][1] = Integer.parseInt(st.nextToken());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                list[a]=b;
             }
-            Arrays.sort(memberList, (o1, o2) -> {
-                return o1[0] - o2[0];
-            });
 
-            int top = Integer.MAX_VALUE;
-            int cnt = 0;
-            for (int[] member : memberList) {
-                if (member[1] < top) {
-                    cnt++;
-                    top = member[1];
-                }
+            int top = list[1];
+            int count = 1;
+
+            for(int i=2; i<=n;i++){
+                if(list[i] > top) continue;
+                count++;
+                top = list[i];
+                if(top == 1) break;
             }
-            sb.append(cnt).append("\n");
+            sb.append(count).append("\n");
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
         br.close();
     }
 }
