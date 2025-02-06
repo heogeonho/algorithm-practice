64f83d3d2285b5cd6b2c6bbcb1c833bf50975425
건호 <111233054+heogeonho@users.noreply.github.com>
2025-02-06 11:01:42 +0900

    [Silver IV] Title: 보물, Time: 192 ms, Memory: 18188 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int N = Integer.parseInt(br.readLine());
+        Integer[] A = new Integer[N];
+        Integer[] B = new Integer[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A, (a, b) ->
+                a - b
+        );
+        Arrays.sort(B, (a, b) ->
+                b - a
+        );
+        int res = 0;
+        for (int i = 0; i < N; i++) {
+            res = res + (A[i] * B[i]);
+        }
+
+        // 최종 결과 출력
+        System.out.println(res);
+        br.close();
+    }
+}
