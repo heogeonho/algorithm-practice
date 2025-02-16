681e5dd08c0c5092625d9b5385da8578f52fa737
건호 <111233054+heogeonho@users.noreply.github.com>
2025-02-17 01:05:21 +0900

    [Silver II] Title: A → B, Time: 88 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,39 +1,40 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        long A = sc.nextLong();
-        long B = sc.nextLong();
-        
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+
         System.out.println(bfs(A, B));
     }
-    
+
     static int bfs(long A, long B) {
-        Queue<long[]> queue = new LinkedList<>();
-        queue.add(new long[]{A, 1});
-        
+        Deque<long[]> queue = new ArrayDeque<>();
+        queue.offer(new long[]{A, 1});
+
         while (!queue.isEmpty()) {
             long[] current = queue.poll();
             long num = current[0];
             int steps = (int) current[1];
-           
-       
+
             if (num == B) {
                 return steps;
             }
-            
+
             if (num * 2 <= B) {
-                queue.add(new long[]{num * 2, steps + 1});
+                queue.offer(new long[]{num * 2, steps + 1});
             }
             if (num * 10 + 1 <= B) {
-                queue.add(new long[]{num * 10 + 1, steps + 1});
+                queue.offer(new long[]{num * 10 + 1, steps + 1});
             }
         }
-        
+
         return -1;
     }
 }
