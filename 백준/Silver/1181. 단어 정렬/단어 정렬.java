1fd598c95b7249f8d19035ac3c6d8fd7984277af
건호 <111233054+heogeonho@users.noreply.github.com>
2025-03-10 23:33:30 +0900

    [Silver V] Title: 단어 정렬, Time: 328 ms, Memory: 32512 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,35 +1,35 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb=new StringBuilder();
-		
-		int N=Integer.parseInt(br.readLine());
-		String[] a=new String[N];
-		for(int i=0; i<N; i++) {
-			a[i]=br.readLine();
-		}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		Arrays.sort(a, (o1, o2)-> {
-			if(o1.length()==o2.length()) {
-				return o1.compareTo(o2);
-			}
-			return o1.length()-o2.length();
-		});
+        int N = Integer.parseInt(br.readLine());
+        String[] a = new String[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = br.readLine();
+        }
 
-		for (int i = 0; i < a.length; i++) {
-			String str = a[i];
-			if(i==0) {
-				sb.append(str).append("\n");				
-			} else {
-				if(!str.equals(a[i-1])) {
-					sb.append(str).append("\n");					
-				}
-			}
-		}
-		System.out.println(sb);
-		br.close();
-	}
+        Arrays.sort(a, (o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            }
+            return o1.length() - o2.length();
+        });
+
+        for (int i = 0; i < a.length; i++) {
+            if (i == 0) {
+                sb.append(a[i]).append("\n");
+                continue;
+            }
+            if (!a[i].equals(a[i - 1])) {
+                sb.append(a[i]).append("\n");
+            }
+        }
+        System.out.println(sb);
+        br.close();
+    }
 }
