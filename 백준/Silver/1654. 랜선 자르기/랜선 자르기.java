527afb7c2e6bd1e1420169cf5574615b46f97a4e
건호 <111233054+heogeonho@users.noreply.github.com>
2025-03-13 23:12:24 +0900

    [Silver II] Title: 랜선 자르기, Time: 116 ms, Memory: 15412 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,50 +1,46 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int K, N;
+    static long[] lines;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        int[] arr = new int[K];
-        long maxLen = Integer.MIN_VALUE;
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        lines = new long[K];
+        long max = 0;
 
         for (int i = 0; i < K; i++) {
-            int input = Integer.parseInt(br.readLine());
-            maxLen = Math.max(input, maxLen);
-            arr[i] = input;
+            lines[i] = Long.parseLong(br.readLine());
+            max = Math.max(max, lines[i]);
         }
 
-        /*
-        N개를 만들 수 있는 랜선의 최대 길이를 구하기
-        -> 특정 길이 이하는 모두 충족인데 이분 탐색을 끝까지 진행해서 가장 큰 값 찾아야 해
-        Upper Bound
-         */
+        long low = 1, high = max + 1;
 
-        // Upper Bound
-        long lo = 0;
-        long mid = 0;
-        long hi = maxLen + 1;
-        while (lo < hi) {
-            mid = (lo + hi) / 2;
-            if (calN(mid, arr) >= N) {
-                lo = mid + 1;
+        while (low < high) {
+            long mid = low + (high - low) / 2;
+
+            if (isCountN(mid)) {
+                low = mid + 1;
             } else {
-                hi = mid;
+                high = mid;
             }
         }
-        System.out.println(lo - 1);
+
+        System.out.println(low - 1);
     }
 
-    static int calN(long len, int[] arr) {
-        int total = 0;
-        for (int v : arr) {
-            total += v / len;
+    static boolean isCountN(long len) {
+        long cnt = 0;
+        for (long l : lines) {
+            cnt += l / len;
         }
-        return total;
+        return cnt >= N;
     }
 }
