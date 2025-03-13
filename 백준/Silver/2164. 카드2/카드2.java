7efc19af2203c2291bf341758ea6ccabeddae054
건호 <111233054+heogeonho@users.noreply.github.com>
2025-03-13 22:17:38 +0900

    [Silver IV] Title: 카드2, Time: 116 ms, Memory: 23868 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,27 +1,30 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int N=Integer.parseInt(br.readLine());
-		ArrayDeque<Integer> q=new ArrayDeque<>();
-		for (int i = 0; i < N; i++) {
-			q.offer(i+1);
-		}
-		int cnt=1;
-		while(!q.isEmpty()) {
-			if(cnt%2==1) {
-				int temp = q.poll();
-				if(q.isEmpty()) {
-					System.out.println(temp);
-					return;
-				}
-			} else {
-				int temp = q.poll();
-				q.offer(temp);
-			}
-			cnt++;
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        int count = 1; // 횟수 카운트
+        // 남은 갯수 1개일 때까지 반복
+        while (queue.size() > 1) {
+            if (count % 2 == 1) {
+                queue.poll();
+            } else {
+                queue.offer(queue.poll());
+            }
+            count++;
+        }
+
+        System.out.println(queue.poll());
+        br.close();
+    }
 }
