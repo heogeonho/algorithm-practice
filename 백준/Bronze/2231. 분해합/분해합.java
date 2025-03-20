6d469fbb79719ff62aa67f5f0dff3d53083a11d5
건호 <111233054+heogeonho@users.noreply.github.com>
2025-03-20 17:33:17 +0900

    [Bronze II] Title: 분해합, Time: 68 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,30 +1,33 @@
 import java.io.*;
- 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int num = Integer.parseInt(str);
+        
+        // 분해합으로 입력을 만들 수 있는 값은 -> 결과 - 자리수 x 9로 판단
+        int findRange = 9 * str.length();
+        int startNum = num - findRange;
+        
+        // 분해합이 나올 수 있는 최대 범위에서 전체 순회
+        int result = 0;
+        for (int i = 0; i < findRange; i++) {
+            if (Integer.parseInt(str) == calDSum(startNum+i)) {
+                result = startNum+i;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
     
-		String str_N = br.readLine();
- 
-		int N_len = str_N.length();
- 
-		int N = Integer.parseInt(str_N);
-		int result = 0;
-		
-		for(int i = (N - (N_len * 9)); i < N; i++) {
-			int number = i;
-			int sum = 0;	// 각 자릿수 합 변수 
-			
-			while(number != 0) {
-				sum += number % 10;	// 각 자릿수 더하기
-				number /= 10;
-			}
-			
-			if(sum + i == N) {
-				result = i;
-				break;
-			}
-		}
-		System.out.println(result);
-	}
+    // 분해합 함수
+    public static int calDSum(int num) {
+        String str = String.valueOf(num);
+        for (char ch : str.toCharArray()) {
+            num+= ch - '1' + 1;
+        }
+        return num;
+    }
 }
