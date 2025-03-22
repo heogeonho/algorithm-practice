2e5e4d2f07b0a3af5dd9c43e1438ef4648492395
건호 <111233054+heogeonho@users.noreply.github.com>
2025-03-22 17:26:20 +0900

    [Silver III] Title: 소수 구하기, Time: 168 ms, Memory: 18692 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,34 +1,42 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb=new StringBuilder();
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st=new StringTokenizer(br.readLine(), " ");
-        int M=Integer.parseInt(st.nextToken());
-        int N=Integer.parseInt(st.nextToken());
+        // M이상 N이하의 소수
+        // 1 ≤ M ≤ N ≤ 1,000,000
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        boolean[] notPrime = new boolean[N+1];
+        notPrime[0] = notPrime[1] = true;
 
-        for(int i = M; i<=N; i++) {
-            if(isPrime(i)){
-                sb.append(i).append("\n");
+        // 에라토스테네스의 채
+        // 잘 기억하고 있을 부분은 범위! 전체 범위의 루트 만큼만 하면 됨
+        for (int i = 2; i * i <= N; i++) {
+            // 이미 걸러진 수는 패스
+            if(notPrime[i]) continue; 
+            
+            // 소수 발견 -> 이후 합성수 필터링
+            int cnt = 2;
+            int p = i * cnt;
+            while (p <= N) {
+                notPrime[p] = true;
+                cnt++;
+                p = i * cnt;
             }
         }
 
+        for (int i = M; i <= N; i++) {
+            if(notPrime[i]) continue;
+            sb.append(i).append("\n");
+        }
         System.out.println(sb.toString());
-    }
-
-    static boolean isPrime(Integer a) {
-        if (a < 2) {
-            return false;
-        }
-
-        for (int i=2; i<=(int) Math.sqrt(a); i++) {
-            if(a%i==0) {
-                return false;
-            }
-        }
-        return true;
+        br.close();
     }
 }
+
+// 확정적인 범위가 있고 소수 여부임을 미리 세팅하고 풀 수 있는 문제에서 활용하면 좋을 듯 하다.
