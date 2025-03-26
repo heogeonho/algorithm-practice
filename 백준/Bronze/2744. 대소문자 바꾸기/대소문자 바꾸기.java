caa24d4fa00050e171922f2e4fc08ef3c3187d4b
건호 <111233054+heogeonho@users.noreply.github.com>
2025-03-27 02:07:03 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 64 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
