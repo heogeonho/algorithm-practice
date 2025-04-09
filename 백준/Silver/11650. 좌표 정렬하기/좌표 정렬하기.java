fd6e58621b3e3a018abf0be97448b5466541e74b
건호 <111233054+heogeonho@users.noreply.github.com>
2025-04-09 22:57:05 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 636 ms, Memory: 60004 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,31 +1,32 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=null;
-		StringBuilder sb=new StringBuilder();
-		
-		int N=Integer.parseInt(br.readLine());
-		int[][] a=new int[N][2];
-		for (int i=0; i<N; i++) {
-			st=new StringTokenizer(br.readLine()," ");
-			a[i][0]=Integer.parseInt(st.nextToken());
-			a[i][1]=Integer.parseInt(st.nextToken());
-		}
-//		for(int[] al:a) System.out.print(Arrays.toString(al)+" ");
-		
-		Arrays.sort(a, (o1,o2)->{
-			int compRes=Integer.compare(o1[0], o2[0]);
-			if (compRes==0) {
-				return Integer.compare(o1[1], o2[1]);
-			}
-			return compRes;
-		});
-		
-		for(int[] al:a) sb.append(al[0]+" "+al[1]).append("\n");
-		System.out.println(sb);
-		br.close();
-	}
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int N = Integer.parseInt(br.readLine());
+        int[][] points = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            points[i][0] = x;
+            points[i][1] = y;
+        }
+
+        // 메인 로직 (정렬)
+        Arrays.sort(points, (o1, o2) -> {
+            if (o1[0] != o2[0]) return o1[0] - o2[0]; // x좌표 오름차순
+            return o1[1] - o2[1]; // y좌표 오름차순
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        for (int[] p : points) {
+            sb.append(p[0]).append(" ").append(p[1]).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
 }
