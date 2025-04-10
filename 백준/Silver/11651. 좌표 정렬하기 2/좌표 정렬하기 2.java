f8ffc308b1c8abacb43440b8aa57b2775b082549
건호 <111233054+heogeonho@users.noreply.github.com>
2025-04-10 13:05:25 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 624 ms, Memory: 60712 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,32 +1,29 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(br.readLine());
         int[][] points = new int[N][2];
-
+        StringTokenizer st = null;
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            points[i][0] = Integer.parseInt(st.nextToken());
-            points[i][1] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            points[i][0] = x;
+            points[i][1] = y;
         }
-
         Arrays.sort(points, (o1, o2) -> {
-            if (o1[1] == o2[1]) {
-                return o1[0] - o2[0];
-            }
-            return o1[1] - o2[1];
+            if (o1[1] != o2[1]) return o1[1] - o2[1];
+            return o1[0] - o2[0];
         });
 
-        for (int[] point : points) {
-            sb.append(point[0]).append(" ").append(point[1]).append("\n");
+        StringBuilder sb = new StringBuilder();
+        for (int[] p : points) {
+            sb.append(p[0]).append(" ").append(p[1]).append("\n");
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
+        br.close(); 
     }
 }
