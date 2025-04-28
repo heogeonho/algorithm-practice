e9ff3b56819fb1951fd88ff79333b7c909a1cc24
건호 <111233054+heogeonho@users.noreply.github.com>
2025-04-29 00:50:31 +0900

    [Silver V] Title: 집합, Time: 916 ms, Memory: 324252 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,35 +1,46 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	static StringBuilder sb=new StringBuilder();
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=null;
-		int M=Integer.parseInt(br.readLine());
-		int S=0;
-		
-		for(int i=0; i<M; i++) {
-			st=new StringTokenizer(br.readLine()," ");
-			String com=st.nextToken();
-			if(com.equals("all")) {
-				S = (1 << 21) - 1;
-			} else if(com.equals("empty")) {
-				S=0;
-			} else {
-				int v=Integer.parseInt(st.nextToken());
-				if(com.equals("add")) {
-					S=S|(1<<v);
-				} else if(com.equals("remove")) {
-					S=S&~(1<<v);
-				} else if(com.equals("check")) {
-					sb.append((S&(1<<v))==0 ? 0: 1).append("\n");
-				} else if(com.equals("toggle")) {
-					S=S^(1<<v);
-				}
-			}
-		}
-		System.out.print(sb.toString());
-		br.close();
-	}
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 연산의 수 M가 3,000,000
+        // 각 연산은  50 
+        // 빠른 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 결과 저장용 StringBuilder
+        StringBuilder sb = new StringBuilder();
+        
+        int M = Integer.parseInt(br.readLine());
+        boolean[] S = new boolean[21]; // 1~20 사용
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+            
+            if (cmd.equals("add")) {
+                int x = Integer.parseInt(st.nextToken());
+                S[x] = true;
+            } else if (cmd.equals("remove")) {
+                int x = Integer.parseInt(st.nextToken());
+                S[x] = false;
+            } else if (cmd.equals("check")) {
+                int x = Integer.parseInt(st.nextToken());
+                sb.append(S[x] ? "1\n" : "0\n");
+            } else if (cmd.equals("toggle")) {
+                int x = Integer.parseInt(st.nextToken());
+                S[x] = !S[x];
+            } else if (cmd.equals("all")) {
+                for (int j = 1; j <= 20; j++) {
+                    S[j] = true;
+                }
+            } else if (cmd.equals("empty")) {
+                for (int j = 1; j <= 20; j++) {
+                    S[j] = false;
+                }
+            }
+        }
+        
+        // 결과 한 번에 출력
+        System.out.print(sb.toString());
+    }
 }
