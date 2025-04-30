279983aa8b62439e0fad2d8ca68006942f1f2e62
건호 <111233054+heogeonho@users.noreply.github.com>
2025-04-30 23:17:51 +0900

    [Silver IV] Title: 듣보잡, Time: 204 ms, Memory: 25492 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -4,38 +4,34 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < N; i++) {
-            String str = br.readLine();
-            map.put(str, 1);
-        }
-        for(int i = 0; i < M; i++) {
-            String str = br.readLine();
-            map.put(str, map.getOrDefault(str, 0) + 1);
+
+        // 듣지 못한 N명 set 처리
+        Set<String> unheard = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            unheard.add(br.readLine());
         }
 
-        // value가 2인 key만 리스트에 추가
+        // 듣보잡
         List<String> result = new ArrayList<>();
-        for(String key : map.keySet()) {
-            if(map.get(key) == 2) {
-                result.add(key);
+
+        // Set에 있는 경우만 리스트에 추가
+        for (int i = 0; i < M; i++) {
+            String name = br.readLine();
+            if (unheard.contains(name)) {
+                result.add(name);
             }
         }
-
-        // 사전순 정렬
+        // 사전 순
         Collections.sort(result);
-        
-        StringBuilder sb = new StringBuilder();
         sb.append(result.size()).append('\n');
-        for(String s : result) {
-            sb.append(s).append('\n');
+        for (String name : result) {
+            sb.append(name).append('\n');
         }
-
         System.out.print(sb);
-        br.close();
+
     }
 }
