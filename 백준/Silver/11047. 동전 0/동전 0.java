d9d758c33f036780bafbbd66ebdd6ff634f1cd5a
건호 <111233054+heogeonho@users.noreply.github.com>
2025-05-02 13:35:10 +0900

    [Silver IV] Title: 동전 0, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,33 +1,28 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine()," ");
-	
-		int N=Integer.parseInt(st.nextToken());
-		int K=Integer.parseInt(st.nextToken());
-	
-		int[] coins=new int[N];
-		
-		for(int i=0; i<N; i++) {
-			coins[i]=Integer.parseInt(br.readLine());
-		}
-//		System.out.println(Arrays.toString(coins));
-		
-		int cnt=0;
-		while(true) {
-			for(int i=N-1; i>=0; i--) {
-				int div=K/coins[i];
-				if(div!=0) {
-					K=K-(coins[i]*(div));
-					cnt+=div;
-				}
-			}
-			if(K==0) break;
-		}
-		System.out.println(cnt);	
-		br.close();
-	}
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] money = new int[N];
+        
+        // 입력을 높은가격순으로 배열에 저장
+        while(N-- > 0) {
+            money[N] = Integer.parseInt(br.readLine());
+        }
+        
+        // 결과 찾기
+        // 큰 금액부터 순회 -> 몫이 0이 아닌 경우 몫에 대한 처리 진행
+        int result = 0;
+        for(int i = 0; i < money.length; i++) {
+            if(money[i] > K) continue;
+            result += K/money[i];
+            K -= money[i]*(K/money[i]);
+            if(K==0) break;
+        }
+        System.out.println(result);
+    }
 }
