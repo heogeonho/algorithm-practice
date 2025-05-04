1855e1ddb63a7ebc06e9ecfde025571628af71d8
건호 <111233054+heogeonho@users.noreply.github.com>
2025-05-04 16:15:24 +0900

    [Silver IV] Title: ATM, Time: 72 ms, Memory: 11840 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,22 +1,28 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));		
-		int N=Integer.parseInt(br.readLine());
-	
-		int[] times=new int[N];
-		StringTokenizer st=new StringTokenizer(br.readLine()," ");
-		for(int i=0; i<N; i++) {
-			times[i]=Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(times);
-		int result=0;
-		for(int i=0; i<N; i++) {
-			result+=((N-i)*times[i]);
-		}
-		System.out.println(result);	
-		br.close();
-	}
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        // 작은 수 먼저 처리할 수 있도록 정렬
+        Arrays.sort(arr);
+        
+        // 시간을 누적하여 소요시간 총 합 산출
+        int res = 0;
+        int temp = 0;
+        for(int i = 0; i < N; i++) {
+            temp += arr[i];            
+            res += temp;
+        }
+        System.out.println(res);
+    }
 }
