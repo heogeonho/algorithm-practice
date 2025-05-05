a8d96e81e6b651da4d3960f220bcf18687d6ff84
건호 <111233054+heogeonho@users.noreply.github.com>
2025-05-05 17:46:14 +0900

    [Silver III] Title: 피보나치 함수, Time: 60 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,39 +1,30 @@
-import java.util.*;
 import java.io.*;
 
+// Bottom-Up 활용 DP
 public class Main {
-	static int cntZero=0, cntOne=0;
-	static int[][] fiboRes;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb=new StringBuilder();
-		
-		int T=Integer.parseInt(br.readLine());
-		
-		fiboRes=new int[41][2]; //0이나 40보다 작거나 같은 자연수의 피보 결과 저장 리스트
-		
-		for(int[] fi:fiboRes) Arrays.fill(fi, -1);
-		
-		 fiboRes[0][0]=1;
-		 fiboRes[0][1]=0;
-		 fiboRes[1][0]=0;
-		 fiboRes[1][1]=1;
-		 
-		for(int i=0; i<T; i++) {
-			int N=Integer.parseInt(br.readLine());
-			cntZero=0;
-			cntOne=0;
-			fibonacci(N);
-			sb.append(fiboRes[N][0]).append(" ").append(fiboRes[N][1]).append("\n");
-		}
-		System.out.println(sb.toString());
-	}
-	 static int[] fibonacci(int n) {
-		 if(fiboRes[n][0]==-1 || fiboRes[n][1]==-1) {
-			 fiboRes[n][0]= fibonacci(n-1)[0]+fibonacci(n-2)[0];
-			 fiboRes[n][1]= fibonacci(n-1)[1]+fibonacci(n-2)[1];
-		 }
-		 
-		 return fiboRes[n];
-	}
+    static int[][] dp = new int[41][2]; // 0 호출 횟수, 1 호출 횟수
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 초기값 설정
+        dp[0][0] = 1; 
+        dp[0][1] = 0;
+        dp[1][0] = 0; 
+        dp[1][1] = 1;
+
+        // DP
+        for (int i = 2; i <= 40; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+        }
+        
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n][0]).append(" ").append(dp[n][1]).append("\n");
+        }
+        System.out.print(sb);
+    }
 }
