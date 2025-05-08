98dd9f211866b59569464d155134c4f1ed41c4aa
건호 <111233054+heogeonho@users.noreply.github.com>
2025-05-08 15:37:47 +0900

    [Silver III] Title: 파도반 수열, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,36 +1,40 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	static long[] dp=new long[101];
+/*
+파도반 수열
+1번째 = 1
+2번째 = 1
+3번째 = 1
+4번째 = 2
+5번째 = 2
+6번째 = 1번 + 5번
+7번째 = 2번 + 6번
+8번째 = 3번 + 7번
+9번째 = 4번 + 8번
+*/ 
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb=new StringBuilder();
-		
-		int T=Integer.parseInt(br.readLine());
-		dp[1]=1;
-		dp[2]=1;
-		dp[3]=1;
-		dp[4]=2;
-		dp[5]=2;
-		
-		for(int i=0; i<T; i++) {
-			sb.append(pdb(Integer.parseInt(br.readLine()))).append("\n");
-		}
-		
-		System.out.println(sb.toString());
-		br.close();
-	}
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        long[] dp = new long[101];
+        
+        // 초기값 설정
+        dp[1] = dp[2] = dp[3] = 1;
+        dp[4] = dp[5] = 2;
 
-	private static long pdb(int a) {
-		if(a>=1 && a<=5) {
-			return dp[a];
-		}
-		if(dp[a]!=0) {
-			return dp[a];
-		}
-        dp[a]=pdb(a-1)+pdb(a-5);
-		return dp[a];
-	}
+        // 바텀업 방식으로 dp 배열 미리 채워넣기
+        for (int i = 6; i <= 100; i++) {
+            dp[i] = dp[i-5] + dp[i-1];
+        }
+
+        // 테스트별로 채워넣은 dp 값 출력
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N]).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
 }
