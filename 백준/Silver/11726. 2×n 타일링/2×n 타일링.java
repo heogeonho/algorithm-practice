ea5440e5a66f8ef5d3d3731916459042a156ea62
건호 <111233054+heogeonho@users.noreply.github.com>
2025-05-11 10:42:51 +0900

    [Silver III] Title: 2×n 타일링, Time: 64 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,20 +1,24 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) throws Exception {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int[] dp=new int[n+1];
-		
-		if (n>=1) dp[1]=1;
-		if (n>=2) dp[2]=2;
-		
-		for (int i=3; i<=n; i++ ) {
-			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-        }
-        
-        // Output the result
-        System.out.println(dp[n]);
+/*
+탑다운 방식을 활용하여 dp 문제 풀어보기
+점화식: dp(n-1) + dp(n-2) = dp(n)
+*/
+
+class Main {
+    static int[] dp = new int[10001]; // 메모이제이션을 위한 1-based arr
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp[1] = 1;
+        dp[2] = 2;  
+        System.out.println(sol(n));
+    }
+
+    // 재귀 함수를 활용해 top-down 형식으로 해 찾기
+    static int sol(int n) {
+        if(dp[n]!=0) return dp[n];
+        return dp[n] = (sol(n-1) + sol(n-2)) % 10007;
     }
 }
