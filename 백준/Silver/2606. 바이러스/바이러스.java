5514a5b7c0440fc04eec2b418ccb5901d376ae90
건호 <111233054+heogeonho@users.noreply.github.com>
2025-05-13 12:50:33 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,60 +1,49 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	static int N, cnt=0;
-	static List<Integer>[] network;
-	static boolean[] v;
+class Main {
+    
+    static int n;
+    static List<Integer>[] network;
+    static boolean[] v;
+    
+    static int vsCnt = 0; // 바이러스 확산 cnt 변수
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=null;
-		
-		N=Integer.parseInt(br.readLine());
-		int E=Integer.parseInt(br.readLine());
-		
-		network=new List[N+1]; for(int i=1; i<N+1; i++) network[i]=new ArrayList<>();
-		v=new boolean[N+1];
-		
-		for(int i=0; i<E; i++) {
-			st=new StringTokenizer(br.readLine()," ");
-			int from=Integer.parseInt(st.nextToken());
-			int to=Integer.parseInt(st.nextToken());
-			network[from].add(to);
-			network[to].add(from);
-		}
-		
-//		dfs(1);
-		bfs(1);
-		System.out.println(cnt);
-		br.close();
-	}
+        n = Integer.parseInt(br.readLine());
+        
+        // 노드별 방문 여부 배열
+        v = new boolean[n+1];
+        
+        // 네트워크 그래프 생성
+        network = new List[n+1];
+        for(int i = 0; i <= n; i++) network[i] = new ArrayList<>();
 
-	private static void bfs(int i) {
-		ArrayDeque<Integer> q=new ArrayDeque<>();
-		v[i]=true;
-		q.offer(i);
-		while(!q.isEmpty()) {
-			i=q.poll();
-			for(int j:network[i]) {
-				if(!v[j]) {
-					cnt++;
-					v[j]=true;
-					q.offer(j);
-				}
-			}
-		}
-		
-	}
+        // 그래프 값 입력 (양방향 그래프)
+        int connect = Integer.parseInt(br.readLine());
+        while (connect-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            network[node1].add(node2);
+            network[node2].add(node1);
+        }
+        dfs(1);
 
-	private static void dfs(int i) {
-		v[i]=true;
-		for(int j:network[i]) {
-			if(!v[j]) {
-				cnt++;
-				dfs(j);
-			}
-		}
-	}
-
+        System.out.println(vsCnt);
+    }
+    
+    // dfs 탐색
+    static void dfs(int i) {
+        v[i] = true;
+        for(int node : network[i]) {
+            if(!v[node]) {
+                vsCnt++;
+                dfs(node);
+            }
+        }
+    }
 }
