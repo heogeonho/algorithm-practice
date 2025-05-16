05b9302eaa9b05a64b5fee6ce9cc86eaf1e44306
건호 <111233054+heogeonho@users.noreply.github.com>
2025-05-16 13:52:03 +0900

    [Silver II] Title: 좌표 압축, Time: 2468 ms, Memory: 289116 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,48 +1,46 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	
-	/*
-	주어진 배열 정렬 (2차원 배열로 인덱스 값까지 저장)
-	
-	새로 만든 2차원 배열 정렬
-	
-	꺼낸 값을 저장하는 (값 꺼내면서 비교 같으면 압축결과 값 늘리지 않고 커지면 늘려서 할당)
-	 */
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb=new StringBuilder();
-		
-		int N=Integer.parseInt(br.readLine());
-		int[][] arr=new int[N][2];
-		StringTokenizer st=new StringTokenizer(br.readLine()," ");
-		
-		for(int i=0; i<N; i++) {
-			arr[i]=new int[] {Integer.parseInt(st.nextToken()),i};
-		}
-		
-		Arrays.sort(arr, (o1, o2) -> {
-			return Integer.compare(o1[0],o2[0]);	
-		});
-		
-//		for(int[] a: arr) System.out.println(Arrays.toString(a));
-		
-		int[] res=new int[N];
-		int cnt=0;
-		int temp=arr[0][0];
-		res[arr[0][1]]=cnt;
-		
-		for(int i=1; i<N; i++) {
-			if(temp!=arr[i][0]) cnt++;
-			res[arr[i][1]]=cnt;
-			temp=arr[i][0];
-		}
-		
-		for(int k:res) sb.append(k).append(" ");
-		
-		System.out.print(sb.toString());
-		br.close();
-	}
+// 2차원 배열을 활용해 인덱스 값을 포함해 정렬 진행
+// 인덱스를 순회하며 앞 숫자와 같은지 판별 후 값 할당 
+// 같은 경우 이전 수 활용, 다른 경우 인덱스 값 활용해 업데이트
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] valueIdxArr = new int[N][2];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            valueIdxArr[i] = new int[] {Integer.parseInt(st.nextToken()), i};
+        }
+
+        Arrays.sort(valueIdxArr, (o1, o2) -> {
+            return o1[0]-o2[0];
+        });
+
+        int[] res = new int[N];
+        int preVal = Integer.MIN_VALUE;
+        int preIdx = -1;
+        int idx = 0;
+        for (int i = 0; i < N; i++) {
+            int curVal = valueIdxArr[i][0];
+            int curIdx = valueIdxArr[i][1];
+            if (preVal != curVal) {
+                res[curIdx] = idx;
+                preVal = valueIdxArr[i][0];
+                preIdx = idx;
+                idx++;
+            } else {
+                res[curIdx] = preIdx;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : res) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
 }
