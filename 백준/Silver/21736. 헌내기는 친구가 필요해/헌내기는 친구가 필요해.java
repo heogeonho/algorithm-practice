7693544be2b5a13c9cf57d2a1b76c5e32731a210
건호 <111233054+heogeonho@users.noreply.github.com>
2025-05-16 16:05:40 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 248 ms, Memory: 25960 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,62 +1,64 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+class Main {
 
     static int N, M;
     static char[][] map;
     static boolean[][] v;
-    static int[] di = new int[]{-1, 0, 1, 0};
-    static int[] dj = new int[]{0, 1, 0, -1};
-    static int result = 0;
+    static final int[] di = new int[] {-1, 0, 1, 0};
+    static final int[] dj = new int[] {0, 1, 0, -1};
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         map = new char[N][M];
         v = new boolean[N][M];
-
-        int xi = 0;
-        int yj = 0;
-
+        int si = 0;
+        int sj = 0;
         for (int i = 0; i < N; i++) {
-            String stLine = br.readLine();
+            String str = br.readLine();
             for (int j = 0; j < M; j++) {
-                char value = stLine.charAt(j);
-                map[i][j] = value;
-                if (value == 'I') {
-                    xi = i;
-                    yj = j;
+                char temp = str.charAt(j);
+                // 시작 위치 저장
+                if (temp == 'I') {
+                    si = i;
+                    sj = j;
                 }
+                map[i][j] = temp;
             }
         }
-
-        dfs(xi, yj);
-
-        if (result != 0) {
-            System.out.println(result);
-        } else {
-            System.out.println("TT");
-        }
+        // 너비우선탐색 활용
+        int res = bfs(si, sj);
+        if(res!=0) System.out.println(res);
+        else System.out.println("TT"); // 0일 경우 TT 출력
     }
 
-    static void dfs(int i, int j) {
+    // bfs 탐색 활용
+    static int bfs(int i, int j) {
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {i, j});
         v[i][j] = true;
-        for (int d = 0; d < 4; d++) {
-            int ni = i + di[d];
-            int nj = j + dj[d];
-            if (0 <= ni && ni < N && 0 <= nj && nj < M && !v[ni][nj] && map[ni][nj] != 'X') {
-                if (map[ni][nj] == 'P') {
-                    result++;
+        
+        int res = 0;
+        while (!q.isEmpty()) {
+            int[] ij = q.poll();
+            i = ij[0];
+            j = ij[1];
+            for(int d = 0; d < 4; d++) {
+                int ni = i + di[d];
+                int nj = j + dj[d];
+                if(0<=ni&&ni<N && 0<=nj&&nj<M && !v[ni][nj] && map[ni][nj]!='X') { // 벽'X' 추가 처리
+                    if(map[ni][nj]=='P') res++;
+                    q.offer(new int[] {ni, nj});
+                    v[ni][nj] = true;
                 }
-                dfs(ni, nj);
             }
         }
+        return res;
     }
+
 }
