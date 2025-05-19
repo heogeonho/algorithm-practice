96d42924e3de99c67401c20cd3884084695f760f
건호 <111233054+heogeonho@users.noreply.github.com>
2025-05-19 18:06:46 +0900

    [Silver II] Title: 최소 힙, Time: 240 ms, Memory: 24680 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,26 +1,33 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb=new StringBuilder();
-        
-		PriorityQueue<Integer> pq=new PriorityQueue<>();
-		
-		int N=Integer.parseInt(br.readLine());
-		
-        // 입력된 값에 따라 우선순위 큐에 삽입 추출 진행
-		for(int i=0; i<N; i++) {
-			int oper=Integer.parseInt(br.readLine());
-			if(oper!=0) {
-				pq.offer(oper);
-			} else {
-				if(pq.peek()!=null)	sb.append(pq.poll()).append("\n");
-				else sb.append(0).append("\n");
-			}
-		}
-		System.out.println(sb.toString());
-		br.close();
-	}
+// 우선순위 큐 (최소값을 계속해서 갱신해야 함, 즉 반복되는 정렬 수행)
+
+// < 0이 입력된 경우 >
+// 배열에 값 x -> 0 출력
+// 배열에 값 o -> 가장 작은 값 출력
+
+// < 자연수 입력된 경우 >
+// 값 삽입
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while (N-- > 0) {
+            String input = br.readLine();
+            if(!input.equals("0")) {
+                pq.offer(Integer.parseInt(input));
+            } else {
+                if (pq.size()==0) {
+                    sb.append("0").append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            }
+        }
+        System.out.println(sb.toString());
+    }
 }
