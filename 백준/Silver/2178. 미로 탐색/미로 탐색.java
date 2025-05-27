de21cfe15086dc81b8f74cde0808404bf2f0e0f1
건호 <111233054+heogeonho@users.noreply.github.com>
2025-05-27 23:43:40 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 12096 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,59 +1,54 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	static int N, M, ans, C;
-	static int[][] a;
-	static boolean[][] v;
-	static int[] di= {-1,0,1,0};
-	static int[] dj= {0,1,0,-1};
+class Main {
+    static int N, M;
+    static char[][] map;
+    static boolean[][] v;
+    static int[] di = new int[] {-1, 0, 1, 0};
+    static int[] dj = new int[] {0, 1, 0, -1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] size = br.readLine().split(" ");
+        N = Integer.parseInt(size[0]);
+        M = Integer.parseInt(size[1]);
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=null;
-		
-		st=new StringTokenizer(br.readLine()," ");
-		N=Integer.parseInt(st.nextToken());
-		M=Integer.parseInt(st.nextToken());
-		
-		a=new int[N][M];
-		v=new boolean[N][M];
-		for(int i=0; i<N; i++) {
-			String str=br.readLine();
-			for(int j=0; j<M; j++) {
-				char value=str.charAt(j);
-				if(value=='1') {
-					a[i][j]=1;
-				}
-				else {
-					a[i][j]=0;
-				}
-			}
-		}
-		ans=0;
-		C=1;
-		bfs(0,0); //출발인덱스 0,0 도착인덱스 n-1, m-1
-		br.close();
-	}
+        map = new char[N][M];
+        v = new boolean[N][M];
 
-	static void bfs(int i, int j) {
-		ArrayDeque<int[]> q=new ArrayDeque<>();
-		v[i][j]=true;
-		q.offer(new int[] {i,j});
-		while(!q.isEmpty()) {
-			int[] ij=q.poll();
-			i=ij[0];
-			j=ij[1];
-			for(int d=0; d<4; d++) {
-				int ni=i+di[d];
-				int nj=j+dj[d];
-				if(0<=ni&&ni<N && 0<=nj&&nj<M && !v[ni][nj] && a[ni][nj]==1) {
-					v[ni][nj]=true;
-					q.offer(new int[] {ni,nj});
-					a[ni][nj]=a[i][j]+1;
-				}
-			}
-		}
-		System.out.println(a[N-1][M-1]);
-	}
+        for (int i = 0; i < N; i++) {
+            String row = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = row.charAt(j);
+            }
+        }
+        
+        System.out.println(bfs(0, 0));
+    }
+
+    static int bfs(int i, int j) {
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        int[] ij = new int[] {i, j, 1};
+        q.offer(ij);
+        v[i][j] = true;
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+            int ci = curr[0];
+            int cj = curr[1];
+            int cd = curr[2];
+
+            for(int d = 0; d < 4; d++) {
+                int ni = ci + di[d];
+                int nj = cj + dj[d];
+                if(0<=ni&&ni<N && 0<=nj&&nj<M && !v[ni][nj] && map[ni][nj]=='1') {
+                    if(ni == N-1 && nj == M-1) {
+                        return cd+1;
+                    }
+                    v[ni][nj] = true;
+                    q.offer(new int[]{ni, nj, cd+1});
+                }
+            }
+        }
+        return -1;
+    }
 }
