3908cad48a1dafa5900b0dea9b13c9cf228463c5
건호 <111233054+heogeonho@users.noreply.github.com>
2025-06-03 18:28:47 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 12540 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,76 +1,76 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	static int N, a=0, b=0;
-	static char[][] map;
-	static boolean[][] vA, vB;
-	
-	static int[] di =new int[] {1,0,-1,0};
-	static int[] dj =new int[] {0,1,0,-1};
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb=new StringBuilder();
-		N=Integer.parseInt(br.readLine());
-		map=new char[N][N];
-		
-		for(int i=0; i<N; i++) {
-			String str=br.readLine();
-			for(int j=0; j<N; j++) {
-				map[i][j]=str.charAt(j);
-			}
-		}
-		vA=new boolean[N][N];
-		vB=new boolean[N][N];
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(!vA[i][j]) {
-					a++;
-					dfsA(i, j);
-				}
-				if (!vB[i][j]) {
-					b++;
-					dfsB(i, j);
-				}
-			}
-		}
-		
-		sb.append(a).append(" ").append(b);
-		System.out.print(sb.toString());
-		br.close();
-	}
+class Main {
 
-	private static void dfsA(int i, int j) {
-		vA[i][j]=true;
-		char value=map[i][j];
-		for (int d=0; d<4; d++) {
-			int ni=i+di[d];
-			int nj=j+dj[d];
-			if(0<=ni&&ni<N && 0<=nj&&nj<N 
-					&& !vA[ni][nj] && map[ni][nj]==value) {
-				dfsA(ni,nj);
-			}
-		}
-	}
-	
-	private static void dfsB(int i, int j) {
-		vB[i][j]=true;
-		char value=map[i][j];
-		for (int d=0; d<4; d++) {
-			int ni=i+di[d];
-			int nj=j+dj[d];
-			if(value=='R' || value=='G') {
-				if(0<=ni&&ni<N && 0<=nj&&nj<N 
-						&& !vB[ni][nj] && (map[ni][nj]=='R' || map[ni][nj]=='G')) {
-					dfsB(ni,nj);
-				}
-			} else {
-				if(0<=ni&&ni<N && 0<=nj&&nj<N 
-						&& !vB[ni][nj] && map[ni][nj]==value) {
-					dfsB(ni,nj);
-				}
-			}
-		}	
-	}
+    static int N;
+    static char[][] map;
+    static boolean[][] v;
+    static boolean[][] vRG;
+    static int cnt, cntRG = 0;
+
+    static final int[] di = new int[] {-1, 0, 1, 0};
+    static final int[] dj = new int[] {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        v = new boolean[N][N];
+        vRG = new boolean[N][N];
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < N; j++) {
+                map[i][j] = str.charAt(j);
+            }
+        }
+
+        // 방문 x인 경우 알파벳 인식
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(!v[i][j])  {
+                    dfs(i, j, map[i][j]);
+                    cnt++;
+                }
+                if(!vRG[i][j]) {
+                    dfsRG(i, j, map[i][j]);
+                    cntRG++;
+                }
+            }
+        }
+
+        System.out.print(cnt+" "+cntRG);
+    }
+
+    static void dfs(int i, int j, char rgb) {
+        v[i][j] = true;
+        for (int d = 0; d < 4; d++) {
+            int ni = i + di[d];
+            int nj = j + dj[d];
+            if(0<=ni&&ni<N && 0<=nj&&nj<N && !v[ni][nj] && map[ni][nj] == rgb) {
+                dfs(ni,nj,rgb);
+            }
+        }
+    }
+
+    static void dfsRG(int i, int j, char rgb) {
+        vRG[i][j] = true;
+        for (int d = 0; d < 4; d++) {
+            int ni = i + di[d];
+            int nj = j + dj[d];
+            if(rgb == 'R' || rgb == 'G') {
+                if(0<=ni&&ni<N && 0<=nj&&nj<N && !vRG[ni][nj]) {
+                    if(map[ni][nj] == 'R' || map[ni][nj] == 'G') {
+                        dfsRG(ni,nj,rgb);
+                    }
+                }
+            }
+            else {
+                if(0<=ni&&ni<N && 0<=nj&&nj<N && !vRG[ni][nj] && map[ni][nj] == rgb) {
+                    dfsRG(ni,nj,rgb);
+                }
+            }
+
+        }
+    }
 }
