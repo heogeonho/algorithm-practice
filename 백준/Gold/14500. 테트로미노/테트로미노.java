3bd6db2ba8c8d8de7b01b0bc208565335412e2bd
건호 <111233054+heogeonho@users.noreply.github.com>
2025-06-08 11:31:12 +0900

    [Gold IV] Title: 테트로미노, Time: 660 ms, Memory: 136824 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,94 +1,89 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+// dfs 예외 모양만 따로 처리하기 구현이 어려웠던 문제
+// 아이디어 좀 더 생각해야 했던 문제
 
 public class Main {
-    static int N, M, max = Integer.MIN_VALUE;
+    static int N, M, max = 0;
     static int[][] map;
     static boolean[][] v;
-    static int[] di = {-1, 0, 1, 0};
-    static int[] dj = {0, 1, 0, -1};
+    static int[] di = {-1, 1, 0, 0};
+    static int[] dj = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
         map = new int[N][M];
         v = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for (int j = 0; j < M; j++)
                 map[i][j] = Integer.parseInt(st.nextToken());
-            }
         }
 
+        // 모든 좌표에서 탐색
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                dfs(i, j, 0, 0);
-                handleSpecialShape(i, j);
-                resetVisited();
+                v[i][j] = true;
+                dfs(i, j, map[i][j], 1);
+                v[i][j] = false;
+                checkT(i, j);
             }
         }
 
         System.out.println(max);
     }
 
-    static void dfs(int i, int j, int cnt, int sum) {
-        v[i][j] = true;
-        sum += map[i][j];
-        if (++cnt == 4) {
+    static void dfs(int i, int j, int sum, int cnt) {
+        if (cnt == 4) {
             max = Math.max(max, sum);
-            v[i][j] = false; // 주의해야 했던 부분
             return;
         }
 
         for (int d = 0; d < 4; d++) {
             int ni = i + di[d];
             int nj = j + dj[d];
-            if (ni >= 0 && ni < N && nj >= 0 && nj < M && !v[ni][nj]) {
-                dfs(ni, nj, cnt, sum);
+
+            if (ni >= 0 && nj >= 0 && ni < N && nj < M && !v[ni][nj]) {
+                v[ni][nj] = true;
+                dfs(ni, nj, sum + map[ni][nj], cnt + 1);
+                v[ni][nj] = false;
             }
         }
-        v[i][j] = false; // 주의해야 했던 부분
     }
 
-    private static void handleSpecialShape(int i, int j) {
+    // DFS로 만들 수 없는 ‘ㅗ’ 모양 직접 체크 !!!
+    static void checkT(int i, int j) {
         int[][][] shapes = {
-                {{0, 0}, {0, 1}, {0, -1}, {-1, 0}}, // ㅗ
-                {{0, 0}, {-1, 0}, {1, 0}, {0, 1}},  // ㅏ
-                {{0, 0}, {0, 1}, {0, -1}, {1, 0}},  // ㅜ
-                {{0, 0}, {-1, 0}, {1, 0}, {0, -1}}  // ㅓ
+            {{0, 0}, {0, 1}, {0, 2}, {1, 1}},   // ㅜ
+            {{0, 0}, {1, 0}, {2, 0}, {1, 1}},   // ㅏ
+            {{0, 0}, {0, 1}, {0, 2}, {-1, 1}},  // ㅗ
+            {{0, 0}, {1, 0}, {2, 0}, {1, -1}}   // ㅓ
         };
 
         for (int[][] shape : shapes) {
             int sum = 0;
-            boolean isValid = true;
+            boolean ok = true;
 
-            for (int[] coord : shape) {
-                int ni = i + coord[0];
-                int nj = j + coord[1];
-                if (ni < 0 || ni >= N || nj < 0 || nj >= M) {
-                    isValid = false;
+            for (int[] b : shape) {
+                int ni = i + b[0];
+                int nj = j + b[1];
+                if (ni >= 0 && nj >= 0 && ni < N && nj < M) {
+                    sum += map[ni][nj];
+                } else {
+                    ok = false;
                     break;
                 }
-                sum += map[ni][nj];
             }
 
-            if (isValid) {
-                max = Math.max(max, sum);
-            }
-        }
-    }
-
-    static void resetVisited() {
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(v[i], false);
+            if (ok) max = Math.max(max, sum);
         }
     }
 }
