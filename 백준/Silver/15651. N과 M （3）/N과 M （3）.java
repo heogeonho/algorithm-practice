78c16cbc3b6a3950be0745099bd25c1eb20e697f
건호 <111233054+heogeonho@users.noreply.github.com>
2025-10-13 10:54:14 +0900

    [Silver III] Title: N과 M (3), Time: 340 ms, Memory: 112308 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,44 +1,39 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	static StringBuilder sb=new StringBuilder();
-	static int N, M;
-	static int[] a;
-	static int[] b;
-//	static boolean[] v;
+class Main {
+    static int N, M; // N 개중 길이 M의 순열 찾기
+    static int[] a, res; 
+    static StringBuilder sb;
 
-	private static void perm(int cnt) {
-		if(cnt==M) {
-			for(int i:b) sb.append(i+" ");
-			sb.append("\n");
-			return;
-		}
-		for(int i=0; i<N; i++) {
-//			if(v[i]) continue;
-//			v[i]=true;
-			b[cnt]=a[i];
-			perm(cnt+1);
-//			v[i]=false;
-		}
-	}
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine()," ");
-		N=Integer.parseInt(st.nextToken());
-		M=Integer.parseInt(st.nextToken());
-		
-		a=new int[N];
-		for (int i = 1; i <= N; i++) {
-			a[i-1]=i;
-		}
-		
-		b=new int[M];
-//		v=new boolean[N];
-		perm(0);
-//		System.out.println(Arrays.toString(a));
-		System.out.println(sb.toString());
-		br.close();
-	}
+    static void perm(int cnt) {
+        if(cnt == M) {
+            for(int i : res) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            res[cnt] = a[i];
+            perm(cnt+1);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = i+1;
+        }
+        res = new int[M];
+
+        sb = new StringBuilder();
+        perm(0);
+        System.out.println(sb.toString());    
+    }
 }
