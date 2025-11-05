27f2623b9343f5641e2a91d932e62bc5cee11b13
건호 <111233054+heogeonho@users.noreply.github.com>
2025-11-05 10:08:27 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 132 ms, Memory: 11676 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -1,21 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
         int result = 0;
-        int size = 1;
-        int criteria = 10;
+        int size = 1;        // 현재 자릿수
+        int criteria = 10;   // 자릿수 경계값 
 
         for (int i = 1; i <= N; i++) {
-            if (i % criteria == 0) {
-                size += 1;
+            if (i == criteria) { // 자릿수 바뀌는 시점
+                size++;
                 criteria *= 10;
             }
-            result += size;
+            result += size;  // 현재 숫자의 자리수를 더함
         }
 
         System.out.println(result);
